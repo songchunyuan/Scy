bf990838f238a03b4a1dc913ca71c075c8ccef2f
Test <1194733981@qq.com>
2018-08-23 22:07:54 +0800

    t

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -2,10 +2,6 @@ package com.yr.Test;
 
 public class Test {
 	public static void main(String[] args) {
-	
-		System.out.println(88888988);
-		System.out.println(666668);
-		System.out.println(666668);
+		System.out.println(6666681);
 	}
-
 }
