bcfe654db6c3acad3b3c37f9dacf8c89d81c9dbe
Test <1194733981@qq.com>
2018-08-23 21:48:32 +0800

    t

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -2,9 +2,9 @@ package com.yr.Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("ËÎ´ºÔª");
 	
-		System.out.println(888888);
+		System.out.println(88888981111);
+		System.out.println(88888981111);
 	}
 
 }
