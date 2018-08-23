050fd7d83869b75c809b8ea8b3c7b45f0f95624d
Test <1194733981@qq.com>
2018-08-23 21:27:08 +0800

    提交

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -2,9 +2,8 @@ package com.yr.Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(111111112);
-		System.out.println(111111999);
-		System.out.println(111111999);
+		System.out.println("ËÎ´ºÔª");
+	
 	}
 
 }
