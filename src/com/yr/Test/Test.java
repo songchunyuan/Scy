e95d43799241eaa7f3ff927696c4e0474316ce7e
songchunyuan <42628766+songchunyuan@users.noreply.github.com>
2018-08-23 21:52:20 +0800

    Update Test.java

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -2,8 +2,7 @@ package com.yr.Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(88888981111);
-		System.out.println(88888981111);
+		System.out.println(88888981119991);
 	}
 
 }
