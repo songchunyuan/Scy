e0707d86f0b82be2c3c4c16213076df4dd578e24
Test <1194733981@qq.com>
2018-08-24 10:26:16 +0800

    提交

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -3,7 +3,8 @@ package com.yr.Test;
 public class Test {
 	public static void main(String[] args) {
 		
-		
+		System.out.println(8888);
+		System.out.println(0000);
 		System.out.println(9999);
 	}
 }
