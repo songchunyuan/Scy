31b67a71fa91cb4b0b1e7f1098323902c23ec567
songchunyuan <42628766+songchunyuan@users.noreply.github.com>
2018-08-24 09:11:22 +0800

    Update Test.java

--- src/com/yr/Test/Test.java
+++ src/com/yr/Test/Test.java
@@ -2,9 +2,7 @@ package com.yr.Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(666888);
-		System.out.println(66688811);
-		System.out.println(666888);
+		System.out.println("宋春元");
 	}
 
 }
